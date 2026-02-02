7f1e223ddca10aa48c6a2b7aed71ffe6bbd075cc
Tomasz Pływacz <tomek@tomaszs-macbook-pro.home>
2026-02-02 15:05:49 +0100

    implemented first version of working organizer

--- src/organizer.py
+++ src/organizer.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 from ai_module import classify_files
 
-def dry_run_simulation(target_path, batch_size=5):
+def create_folder(target_path: Path, category_name: str, files: list):
+    print(f"Creating folder: {category_name}")
+    new_folder = Path(target_path) / category_name
+    new_folder.mkdir(exist_ok=True)
+
+    for file in files:
+        new_path = new_folder / file.name
+        file.rename(new_path)
+    
+def dry_run_simulation(target_path: Path, batch_size=5):
     folder = Path(target_path)
     files_paths = []
     categories = {}
     if not folder.exists() or not folder.is_dir():
-        print(f"BŁĄD: Ścieżka {target_path} nie jest poprawnym folderem.")
+        print(f"Error: {target_path} is not a valid directory.")
         return
 
 
@@ -17,9 +26,19 @@ def dry_run_simulation(target_path, batch_size=5):
     for i in range(0, len(files_paths), batch_size):
         file_batches.append(files_paths[i:i+batch_size])
     for i in range(len(file_batches)):
-        for j in range(len(file_batches[i])):
-            print(file_batches[i][j].name)
-        print(classify_files(file_batches[i]))
+        raw_text = classify_files(file_batches[i])
+        af_text = [category.strip() for category in raw_text.split(",")]
+        if len(af_text) != len(file_batches[i]):
+            print("Error wrong number of categories")
+            return
+        for j, category in enumerate(af_text):
+            if category not in categories:
+                categories[category] = []
+            categories[category].append(file_batches[i][j]) 
+    
+    for category, files in categories.items():
+        create_folder(target_path, category, files)
+
 if __name__ == "__main__":
-    path_to_clean = "/Users/tomek/Nsync/ss"
+    path_to_clean = "/Users/tomek/Nsync/Test Folder"
     dry_run_simulation(path_to_clean)
